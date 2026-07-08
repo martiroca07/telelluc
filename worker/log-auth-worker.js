1e73891394eb09c7b8ce8613db05eb3dbebb1f46
martiroca07 <mrocadlectric@gmail.com>
2026-07-08 08:58:53 +0200

    v0.0.16

--- worker/log-auth-worker.js
+++ worker/log-auth-worker.js
@@ -1,4 +1,5 @@
-const ONLINE_WINDOW_MS = 45 * 1000;
+const HEARTBEAT_INTERVAL_SECONDS = 60;
+const ONLINE_WINDOW_MS = HEARTBEAT_INTERVAL_SECONDS * 2 * 1000;
 
 export default {
     async fetch(request, env) {
